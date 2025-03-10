73c282723e46f7785a25c8f7edab8cc6b73524f0
Нина <nina.lyagusheva.03@bk.ru>
2025-03-10 11:06:13 +0600

    Добавили сервис для входа и заготовку юнит тестов для него

--- src/AuthService/Controllers/AuthController.cs
+++ src/AuthService/Controllers/AuthController.cs
@@ -1,19 +1,16 @@
 #pragma warning disable CA1515
 
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using AuthService.Models;
+using AuthService.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace AuthService.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public sealed class AuthController (IConfiguration configuration) : ControllerBase
+public sealed class AuthController (LoginService loginService) : ControllerBase
 {
-	private readonly IConfiguration _configuration = configuration;
+	//private readonly LoginService _loginService = loginService;
 
 	[HttpPost("login")]
 	public IActionResult Login ([FromBody] User user)
@@ -22,35 +19,16 @@ public sealed class AuthController (IConfiguration configuration) : ControllerBa
 		{
 			return BadRequest();
 		}
-		else if (user.Username == "user" && user.Password == "user")
-		{
-			// Генерация JWT-токена
-			string token = GenerateJwtToken(user.Username);
 
+		try
+		{
+			string token = loginService.Login(user.Username, user.Password);
 			Response.Cookies.Append("jwt", token);
-			return Ok(token);
+			return Ok();
 		}
-		else
+		catch (Exception)
 		{
 			return Unauthorized();
 		}
-	}
-
-	private string GenerateJwtToken (string username)
-	{
-		byte [] key = Encoding.UTF8.GetBytes(_configuration ["Jwt:Key"]);
-
-		JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-
-		SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
-		{
-			Subject = new ClaimsIdentity(new [] { new Claim(ClaimTypes.Name, username) }),
-			Expires = DateTime.UtcNow.AddHours(1),
-			SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-		};
-
-		SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
-		string tokenString = tokenHandler.WriteToken(token);
-		return tokenString;
 	}
 }
---
+++ src/AuthService/Services/LoginService.cs
@@ -0,0 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Services;
+
+public class LoginService (IConfiguration configuration)
+{
+	private readonly IConfiguration _configuration = configuration;
+	public string Login (string username, string password)
+	{
+		if (username == "user" && password == "user")
+		{
+			// Генерация JWT-токена
+			string token = GenerateJwtToken(username);
+
+			return token;
+		}
+		else
+		{
+			throw new UnauthorizedAccessException();
+		}
+	}
+
+	private string GenerateJwtToken (string username)
+	{
+		byte [] key = Encoding.UTF8.GetBytes(_configuration ["Jwt:Key"]);
+
+		JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+		SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+		{
+			Subject = new ClaimsIdentity(new [] { new Claim(ClaimTypes.Name, username) }),
+			Expires = DateTime.UtcNow.AddHours(1),
+			SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+		};
+
+		SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+		string tokenString = tokenHandler.WriteToken(token);
+		return tokenString;
+	}
+}

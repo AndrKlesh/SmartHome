970e0f0d87342484e4983551994d980cc6c61d18
Нина <nina.lyagusheva.03@bk.ru>
2025-03-31 10:38:12 +0600

    удалила комментарии

--- src/AuthService/Program.cs
+++ src/AuthService/Program.cs
@@ -1,7 +1,5 @@
 using System.Text;
 using AuthService.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 namespace AuthService;
 
@@ -15,44 +13,13 @@ internal sealed class Program
 		_ = builder.Services.AddEndpointsApiExplorer();
 		_ = builder.Services.AddAuthorization();
 		_ = builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-		/*_ = builder.Services.AddCors(options =>
-		{
-			options.AddPolicy("AllowFrontend",
-				policy =>
-				{
-					_ = policy.WithOrigins("http://localhost:5173")  // !!!Разрешаем запросы с фронта!!!
-						  .AllowAnyMethod()
-						  .AllowAnyHeader()
-						  .AllowCredentials();
-				});
-		});*/
 
 		IConfigurationSection jwtSettings = builder.Configuration.GetSection("Jwt");
 		byte [] key = Encoding.UTF8.GetBytes(jwtSettings ["Key"]);
 
-		/*_ = builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuer = true,
-			ValidateAudience = true,
-			ValidateLifetime = true,
-			ValidateIssuerSigningKey = true,
-			ValidIssuer = jwtSettings ["Issuer"],
-			ValidAudience = jwtSettings ["Audience"],
-			IssuerSigningKey = new SymmetricSecurityKey(key)
-		};
-	});*/
-
 		WebApplication app = builder.Build();
 
-		//_ = app.UseCors("AllowFrontend");
 		_ = app.UseCors("AllowAll");
-
-		//_ = app.UseAuthentication();
-
-		//_ = app.UseAuthorization();
 
 		_ = app.MapControllers();
 

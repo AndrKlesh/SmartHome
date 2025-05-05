a35148058d8fac318647732f81c23294a053e3ef
Клещенок Андрей Анатольевич <klecshenok@mir-omsk.ru>
2025-05-05 11:56:36 +0600

    Переход на http (временно)

--- src/SmartHomeGUI/src/Config.ts
+++ src/SmartHomeGUI/src/Config.ts
@@ -1,3 +1,3 @@
 //TODO: Use .env files
 export const BASE_NAME = ""
-export const API_BASE_URL = 'https://' + location.hostname + ':' + '7098' + BASE_NAME + '/api'
+export const API_BASE_URL = 'http://' + location.hostname + ':' + '7098' + BASE_NAME + '/api'
